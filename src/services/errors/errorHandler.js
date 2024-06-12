cbe4bb797b684d7d642a31d99d48a83113ebe41d
VeroPizzato <verizzato@gmail.com>
2024-06-12 14:02:25 -0300

    first commit

---
+++ src/services/errors/errorHandler.js
@@ -0,0 +1,32 @@
+const { ErrorCodes } = require("./errorCodes")
+/**
+ * @type {import("express").ErrorRequestHandler} *
+ */
+
+const errorHandler = (error, req, res, next) => {
+    console.log(error.cause);
+    switch (error.code) {
+        case ErrorCodes.INVALID_TYPES_ERROR:
+            res.status(400).send({ status: 'error', error: error.name, cause: error.cause })
+            break;
+        case ErrorCodes.INVALID_PARAM_ERROR:
+            res.status(400).send({ status: 'error', error: error.name, cause: error.cause })
+            break;
+        case ErrorCodes.DATABASE_ERROR:
+            res.status(500).send({ status: 'error', error: error.name, cause: error.cause })
+            break;
+        case ErrorCodes.ROUTING_ERROR:
+            res.status(500).send({ status: 'error', error: error.name, cause: error.cause })
+            break;
+        case ErrorCodes.NOT_FOUND:
+            res.status(404).send({ status: 'error', error: error.name, cause: error.cause })
+            break;
+        default:
+            res.status(500).send({ status: 'error', error: "Unhandled error" })
+            break;
+    }
+
+    next(error)
+}
+
+module.exports = { errorHandler }
